f44fb339ba17e9e8b8d7f1d7034086d1578ada9a
gearnode <bryan@frimin.fr>
2024-04-17 11:27:48 +0200

    Fix transform.sh
    Signed-off-by: gearnode <bryan@frimin.fr>

--- transform.sh
+++ transform.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 DIST_DIR="./dist"
-OUTPUT_DIR="./dist"
 
 find "$DIST_DIR" -name 'index.html' | while read -r filepath; do
-    newpath=$(echo "$filepath" | sed -e "s|^$DIST_DIR/||" -e 's|/index.html||')
-    newfilename=$(echo "$newpath" | tr '/' '-')
-    cp "$filepath" "$OUTPUT_DIR/$newfilename.html"
+    dirpath=$(dirname "$filepath")
+    parentdir=$(dirname "$dirpath")
+    basedirname=$(basename "$dirpath")
+    newfilename="${basedirname}.html"
+    cp "$filepath" "$parentdir/$newfilename"
 done
 
